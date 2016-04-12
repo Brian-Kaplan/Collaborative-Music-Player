4ba03096ea01bbfd587432e11384cdf422fa0517
Luke Mazzu <mluke94@vt.edu>
2016-04-11 21:59:08 -0400

    front end development

---
+++ HTML5Application/public_html/main.js
@@ -0,0 +1,44 @@
+/* 
+ * Javascript file to run the room html page
+ * Created By: Luke Mazzu
+ */
+
+//global variables
+    var submitbutton;
+    var songinput;
+    var player;
+    var songqueue;
+
+$(document).ready(function() {
+    //init variables
+    submitbutton = document.getElementById('submitbutton');
+    songinput = document.getElementById('songinput');
+    player = document.getElementById('player');
+    songqueue = $('#songqueue')[0];
+
+//onclick listener
+submitbutton.onclick = function() {
+    if(player.src === '') {
+        var embedurl = "https://w.soundcloud.com/player/?url="+ songinput.value +"&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false";
+        player.src = embedurl;
+        songinput.value = '';
+    }
+    else {
+        queueSong(0,songinput.value,'00:00');
+        songinput.value = '';
+    }
+};      
+});
+
+//songname currently is song url from soundcloud
+function queueSong(credits,songname,length) {
+        newRow = songqueue.insertRow(-1);
+        newRow.insertCell(0).innerHTML = 0;
+        newRow.insertCell(1).innerHTML = songinput.value;
+        newRow.insertCell(2).innerHTML = '00:00';
+}
+
+function dequeSong() {
+    songqueue.deleteRow(0);
+}
+
