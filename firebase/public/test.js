2be4b919679164c8cd1dd32b53fecc1f6f9be04c
colingray <colinag@vt.edu>
2016-04-25 18:22:27 -0400

    sdfasdfs

--- firebase/public/test.js
+++ firebase/public/test.js
@@ -7,6 +7,11 @@ function runTests() {
    if (clearTableTest()) {
    		console.log("clearTableTest Passed");
    }
+   if (testDequeSong()) {
+   		console.log("testDequeSong: pass")
+   }
+   testIsValidSongTrue();
+   testIsValidSongFalse();
 }
 
 function clearTableTest() {
@@ -27,3 +32,36 @@ function clearTableTest() {
 	return false;
 }
 
+function testIsValidSongTrue() {
+	if (isValidSong('https://soundcloud.com/adamfostermusic/adam-foster-feat-monty-wells-daylight')) {
+		console.log("Test isValidSong with valid input: pass");
+	} else {
+		console.log("Test isValidSong with valid input: fail")
+	}
+}
+
+function testIsValidSongFalse() {
+	if (isValidSong('https://www.thisshouldntwork.com/')) {
+		console.log("Test isValidSong with bad input: fail");
+	} else {
+		console.log("Test isValidSong with bad input: pass");
+	}
+}
+
+function testDequeSong() {
+	songTable = document.getElementById("songqueue");
+	console.log("songTable length:" + songTable.rows.length);
+	//newRow = songTable.insertRow(-1);
+	//newRow.insertCell(0).innerHTML = "test";
+
+	console.log("songTable length after add:" + songTable.rows.length);
+	loadPlayer('https://soundcloud.com/adamfostermusic/adam-foster-feat-monty-wells-daylight');
+	queueSong("0",'https://soundcloud.com/adamfostermusic/adam-foster-feat-monty-wells-daylight',"00:00");
+	dequeSong();
+	console.log("songTable length after dequeSong() called:" + songTable.rows.length);
+	if (songTable.rows.length == 1) {
+		console.log("songTable is empty");
+		return true;
+	}
+	return false;
+}
